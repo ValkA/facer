5ecd050e1ee3d0b984d3d98b180f63ec26fef354
ValkA <valentin.dashinsky@il.ibm.com>
2018-10-17 14:16:16 +0300

    working demo

---
+++ main.py
@@ -0,0 +1,71 @@
+import dlib
+import cv2
+import json
+
+
+predictor_path = "shape_predictor_5_face_landmarks.dat"
+face_rec_model_path = "dlib_face_recognition_resnet_model_v1.dat"
+faces_folder_path = "data"
+
+with open('./data/data.json') as f:
+    data = json.load(f)
+faces = data['faces']
+
+detector = dlib.get_frontal_face_detector() #a detector to find the data
+sp = dlib.shape_predictor(predictor_path) #a shape predictor to find face landmarks so we can precisely localize the face
+facerec = dlib.face_recognition_model_v1(face_rec_model_path) #face recognition model
+
+video_capture = cv2.VideoCapture(0) #Webcam object
+video_capture.set(3, 320)
+video_capture.set(4, 240)
+
+
+def euclidean_dist(vector_x, vector_y):
+    if len(vector_x) != len(vector_y):
+        raise Exception('Vectors must be same dimensions')
+    return sum((vector_x[dim] - vector_y[dim]) ** 2 for dim in range(len(vector_x)))
+
+def get_face_data(frame, d):
+    shape = sp(frame, d)
+    face1desc = facerec.compute_face_descriptor(frame, shape)  # 100
+    face1desc = list(face1desc[dim] for dim in range(len(face1desc))) #convert to python tuple
+
+    for face2 in faces:
+        if euclidean_dist(face1desc, face2["desc"]) < 0.36:
+            return face2
+    else: #uncomment this to do learning
+        return None
+    name = input("Enter name of the face")
+    image_name = name+".png"
+    faces.append({"desc":face1desc ,"name": name, "image": image_name})
+
+    x, y, w, h = [d.left(), d.top(), d.width(), d.height()]
+    cv2.imwrite("./data/faces/"+image_name, frame[y:y+h, x:x+w])
+    return faces[-1]
+
+while True:
+    ret, frame = video_capture.read()
+    dets = detector(frame, 1)
+    print("Number of data detected: {}".format(len(dets)))
+
+    # Now process each face we found.
+    for k, d in enumerate(dets):
+        # print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(  k, d.left(), d.top(), d.right(), d.bottom()))
+        # Get the landmarks/parts for the face in box d.
+
+        face_data = get_face_data(frame, d)
+        if face_data is None:
+            continue
+
+        cv2.putText(frame, face_data["name"], (d.left(), d.top()), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        if "message" in face_data:
+            cv2.putText(frame, face_data["message"], (d.left(), d.top()+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+
+    cv2.imshow("image", frame) #Display the frame
+    if cv2.waitKey(1) & 0xFF == ord('q'): #Exit program when the user presses 'q'
+        break
+
+with open('./data/data.json', 'w+') as f:
+    json.dump(data, f)
+
+
